8884d47959aff7a89a69631059351595dafe5983
devendralattu <dev12@umbc.edu>
2016-07-28 03:18:01 -0400

    First Commit

---
+++ assets/js/angularFile.js
@@ -0,0 +1,210 @@
+angular.module('myWebpage', [])
+    .directive('education', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_education.html'
+        };
+    })
+    .directive('aboutMe', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/aboutMe.html'
+        };
+    })
+    .directive('profilePic', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/header1_profilePic.html'
+        };
+    })
+    .directive('nameCollege', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/header2_name-college.html'
+        };
+    })
+    .directive('iconLinks', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/header2_iconLinks.html'
+        };
+    })
+    .directive('resumeLinks', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/header3_resumeLinks.html'
+        };
+    })
+    .directive('skillsList', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_skillsList.html'
+        };
+    })
+    .directive('basicInfo', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_basicInfo.html'
+        };
+    })
+    .directive('testimonials', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_testimonials.html'
+        };
+    })
+    .directive('languages', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_languages.html'
+        };
+    })
+    .directive('favouriteCodingMusic', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_favouriteCodingMusic.html'
+        };
+    })
+    .directive('conferences', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_conferences.html'
+        };
+    })
+    .directive('credits', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_credits.html'
+        };
+    })
+    .directive('latestProjects', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/left_latestProjects.html'
+        };
+    })
+    .directive('latestAndHighlighted', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/latestProj/latestAndHighlighted.html'
+        };
+    })
+    .directive('latest1', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/latestProj/latest-1.html'
+        };
+    })
+    .directive('latest2', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/latestProj/latest-2.html'
+        };
+    })
+    .directive('latest3', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/latestProj/latest-3.html'
+        };
+    })
+
+    .directive('otherProjects', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_otherProjects.html'
+        };
+    })
+    .directive('otherProjects1', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/otherProj/other-proj-1.html'
+        };
+    })
+
+    .directive('activities', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_activities.html'
+        };
+    })
+    .directive('activities1', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/Activities/activities1.html'
+        };
+    })
+    .directive('activities2', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/Activities/activities2.html'
+        };
+    })
+    .directive('activities3', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/Activities/activities3.html'
+        };
+    })
+
+    .directive('achievements', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/right_achievements.html'
+        };
+    })
+    .directive('achievements1', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/Achievements/achievements1.html'
+        };
+    })
+    .directive('achievements2', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/Achievements/achievements2.html'
+        };
+    })
+    .directive('achievements3', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/Achievements/achievements3.html'
+        };
+    })
+
+    .directive('workExperience', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/left_workExperience.html'
+        };
+    })
+    .directive('pebstoneCompany', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/workExp/PebstoneInfotech.html'
+        };
+    })
+
+    .directive('propellumCompany', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/workExp/PropellumInfotech.html'
+        };
+    })
+
+    .directive('sparksoftCompany', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/workExp/SparksoftCorporation.html'
+        };
+    })
+
+    .directive('footerSection', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'assets/html/footer-section.html'
+        };
+    })
+
+
+
+    
